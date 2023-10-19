42001c0d1bc912194bff17160f0ce9c45fe8b0fe
lilli <lilliancrabbit@gmail.com>
2023-10-19 16:36:32 -0600

    Initial Commit

---
+++ Pangea/PangeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PangeaApp.swift
+//  Pangea
+//
+//  Created by Lillian Cain on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PangeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
