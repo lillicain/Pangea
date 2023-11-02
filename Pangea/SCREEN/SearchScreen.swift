8b0b3bbf78902c65615e8a05d176c0b9e8669520
lilli <lilliancrabbit@gmail.com>
2023-11-01 20:38:41 -0600

    Views

--- Pangea/SCREEN/SearchScreen.swift
+++ Pangea/SCREEN/SearchScreen.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct SearchScreen: View {
+    
+    @EnvironmentObject var authenticationViewModel: AuthenticationViewModel
+    
     @StateObject var searchViewModel = SearchViewModel()
     
     @State var searchText = ""
@@ -18,18 +21,42 @@ struct SearchScreen: View {
                 LazyVStack(spacing: 35) {
                     ForEach(searchViewModel.users, id: \.username) { user in
                         NavigationLink(destination: Screen(user: user)) {
-                            HStack {
-                                ProfileImageManager(user: user, size: .small)
-                                VStack(alignment: .leading) {
-                                    Text(user.username)
-                                    
-                                    if let name = user.name {
-                                        Text(name)
+                            
+                            VStack {
+                                RoundedRectangle(cornerRadius: 25, style: .circular)
+                                    .foregroundColor(authenticationViewModel.green[0])
+                                    .frame(width: 350, height: 95)
+                                
+                                    .overlay {
+                                        ZStack {
+                                            HStack {
+                                                ProfileImageManager(user: user, size: .small)
+                                                
+                                                VStack(alignment: .leading) {
+                                                    Text(user.username)
+                                                        .fontWeight(.semibold)
+                                                        .kerning(2.5)
+                                                    
+                                                    if let name = user.name {
+                                                        Text(name)
+                                                    }
+                                                }
+                                            }
+                                            .padding()
+                                            .background(.white)
+                                            .frame(width: 325, height: 82.5, alignment: .leading)
+                                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                                        }
+                                        .padding()
+                                        .background(.white)
+                                        .frame(width: 325, height: 82.5)
+                                        .clipShape(RoundedRectangle(cornerRadius: 25))
+                                        
+                                        Spacer()
                                     }
-                                }
-                                Spacer()
+                    
                             }
-                            .padding(.horizontal)
+                           
                         }
                     }
                     .onChange(of: searchText, { oldValue, newValue in
