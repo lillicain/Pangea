6c56a9bee52f442e1f4b7df1b312265e26d2c209
lilli <lilliancrabbit@gmail.com>
2023-11-01 19:45:39 -0600

    Extension

--- Pangea/PangeaApp.swift
+++ Pangea/PangeaApp.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 import FirebaseCore
 import NavigationRouter
-import FirebaseFirestore
-import FirebaseFirestoreSwift
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
@@ -29,41 +27,16 @@ struct PangeaApp: App {
     
     @StateObject var authenticationViewModel = AuthenticationViewModel()
     
-//    @StateObject var serviceManager = ServiceManager()
-    
     var body: some Scene {
         WindowGroup {
             NavigationRouter {
                 Group {
-                    
                     if authenticationViewModel.userSession == nil {
                         PangeaView()
-                      
+                        
                     } else {
-                       
                         Screen()
-//                            .modifier(DarkModeViewModifier())
-                        
-                        
-                        
-//                    if authenticationViewModel.userSession == nil {
-//                        PangeaView()
-//                     
-//                     
-//                    } else {
-//                      
-//                        Screen()
-//                            .modifier(DarkModeViewModifier())
-                        
-//                        PangeaView()
-//                        
-//                        if let user = authenticationViewModel.currentUser {
-//                            Screen(user: user)
-//                            
-//                        } else {
-//                            Screen(user: authenticationViewModel.currentUser ?? User.MOCK_USER[0])
-//                                .modifier(DarkModeViewModifier())
-//                        }
+                            .modifier(DarkModeViewModifier())
                     }
                 }
             }
