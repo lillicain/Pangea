f136c851ccbc12bc05a819cfb983e2b18cf7c4e5
lilli <lilliancrabbit@gmail.com>
2023-11-03 09:35:41 -0600

    View

--- Pangea/VIEW/Location/MapInformation.swift
+++ Pangea/VIEW/Location/MapInformation.swift
@@ -27,3 +27,35 @@ struct MapInformation: View {
         .mapScope(mapScope)
     }
 }
+
+struct Information: View {
+    @Binding var selectedResult: MKMapItem
+    @Binding var lookAroundScene: MKLookAroundScene?
+    
+    var body: some View {
+        LookAroundPreview(initialScene: lookAroundScene)
+            .overlay(alignment: .bottomTrailing) {
+                HStack {
+                    Text(selectedResult.name ?? selectedResult.description)
+                    
+                }
+            }
+            .onAppear {
+                fetchLookAroundPreview()
+            }
+            .onChange(of: selectedResult) { oldValue, newValue in
+                fetchLookAroundPreview()
+            }
+    }
+}
+
+extension Information {
+    
+    func fetchLookAroundPreview() {
+            lookAroundScene = nil
+            Task {
+                let request = MKLookAroundSceneRequest(mapItem: selectedResult)
+                lookAroundScene = try? await request.scene
+        }
+    }
+}
