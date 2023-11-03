4e6a3a100eae89068877319044b35c781e93f908
lilli <lilliancrabbit@gmail.com>
2023-11-03 12:50:59 -0600

    Yay

--- Pangea/SCREEN/Screen.swift
+++ Pangea/SCREEN/Screen.swift
@@ -23,17 +23,14 @@ struct Screen: View {
                     ZStack(alignment: .top) {
                         RoundedRectangle(cornerRadius: 50, style: .circular)
                             .foregroundColor(authenticationViewModel.blue[0])
-                            .frame(width: 412.5, height: 260)
-                        //                            .padding(.bottom, 50)
-                        
-                        
+                            .frame(width: 405, height: 255)
+                            .padding(.top, 5)
+                    
                         ZStack {
                             LocationView()
                                 .frame(width: 400, height: 250, alignment: .top)
-                            
                                 .clipShape(RoundedRectangle(cornerRadius: 50, style: .circular))
                         }
-                        
                         
                         ZStack {
                             Circle()
@@ -48,24 +45,37 @@ struct Screen: View {
                         .background(.white)
                         .clipShape(.circle)
                         .padding(.top, 75)
-                        
                     }
-                    
+                    .overlay {
+                        ZStack {
+                            if user.isCurrentUser {
+                                NavigationLink {
+                                    EditScreen(user: user)
+                                    
+                                } label: {
+                                    Text("Edit Profile")
+                                        .modifier(PostViewModifier())
+                                }
+                                .padding(.leading, 205)
+                                .padding(.bottom, 225)
+                            }
+                        }
+                    }
+             
                     ZStack {
                         VStack(spacing: 7.5) {
                             Text(user.username)
                                 .font(FontEight.title)
                                 .foregroundColor(authenticationViewModel.blue[0])
-                             
                                 .offset(x: -1.5, y: 1.5)
                                 .kerning(1.5)
-//                                .overlay {
-//                                    Text(user.username)
-//                                        .font(FontOne.title)
-//                                        .kerning(1.25)
-//                                        .foregroundColor(authenticationViewModel.green[0])
-//                                    
-//                                }
+                            //                                .overlay {
+                            //                                    Text(user.username)
+                            //                                        .font(FontOne.title)
+                            //                                        .kerning(1.25)
+                            //                                        .foregroundColor(authenticationViewModel.green[0])
+                            //
+                            //                                }
                             if let name = user.name {
                                 Text(name)
                                     .font(FontSeven.large)
@@ -85,41 +95,28 @@ struct Screen: View {
                             }
                             
                             HStack {
-                                //                                UserInformation(value: 1, title: "Post")
-                                //                                UserInformation(value: 1, title: "Post")
-                                
+                                UserInformation(value: 1, title: "Post")
+                                    .padding()
+                                    .padding(.vertical, 50)
+                                    
                                 Spacer()
                                 
-                                if user.isCurrentUser {
-                                    NavigationLink {
-                                        EditScreen(user: user)
-                                        
-                                    } label: {
-//                                        RoundedRectangle(cornerRadius: 25, style: .circular)
-//                                            .foregroundColor(authenticationViewModel.green[0])
-//                                            .frame(width: 175, height: 57.5)
-//                                            .padding(.bottom, 250)
-//                                            .overlay {
-                                                Text("Edit Profile")
-                                            .modifier(PostViewModifier())
-//                                                    .padding(12.5)
-//                                                    .background(.white)
-//                                                    .font(FontOne.small)
-//                                                    .foregroundColor(authenticationViewModel.blue[0])
-//                                                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .circular))
-//                                                    .padding(.bottom, 250)
-//                                            }
-                                    }
-                                    .padding(.trailing)
-                                }
+                        
+                             
                             }
-                            .padding()
                             
-                            Divider()
+                           Rectangle()
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 5)
+                            
+                                .foregroundColor(authenticationViewModel.blue[0])
+                            
                             
                             Spacer()
                         }
                     }
+                    
+               
                     
                     
                     VStack {
@@ -129,7 +126,7 @@ struct Screen: View {
             }
             .padding()
             .background(LinearGradient(colors: [.clear, .clear, .clear, .clear, authenticationViewModel.violet[0].opacity(0.15)], startPoint: .top, endPoint: .bottom))
-          
+            
             .toolbarBackground(.ultraThinMaterial.opacity(0.05), for: .tabBar)
             .toolbarBackground(.ultraThinMaterial.opacity(0.05), for: .navigationBar)
             
@@ -137,7 +134,7 @@ struct Screen: View {
             
             LocationView()
                 .tabItem { Image(systemName: "mappin.and.ellipse.circle.fill") }
-       
+            
             AllFeedView(post: Post.MOCK_POST[0])
                 .tabItem { Image(systemName: "globe.americas") }
             
