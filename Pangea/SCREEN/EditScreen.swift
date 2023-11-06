18d11f8dc70dfe8fe00994368f77254c6efe14b4
lilli <lilliancrabbit@gmail.com>
2023-11-06 07:10:53 -0700

    View colors

--- Pangea/SCREEN/EditScreen.swift
+++ Pangea/SCREEN/EditScreen.swift
@@ -27,10 +27,7 @@ struct EditScreen: View {
     @State var size: CGFloat = 17.5
     @State var backgroundColor = AuthenticationViewModel().backgroundColor
     
-    @State var backgroundColorSelected = ["2D00F7", "C1FF00", "FF206E", "480CA8", "FF5714"]
-    
-    //["ed4519", "8c00ff","ff0000","0000ff", "ff8300","ffff00", "2d00f7", "89fc00","f20089","ff006e", "a4f603", "C1FF00", "000000", "FFA617", "DD1F9F", "990DCE","243838", "B2FA63", "FF7833", "B2A1FF", "F3EDE1", "F85D32", "FC72AB", "D6D8F1", "19736B", "F4A44E", "455054", "308695", "D45769", "E69D45", "D4CFC9", "F2BB13", "442F73", "F2C2DC", "F26C1F", "FB2850", "FF006E", "80B918", "DDDF00", "F38375", "A5BE00", "1E96FC", "D264B6", "FF499E", "89FC00", "00E9D8", "F20089", "7014F2", "FF0000"] "ADFF02"
-    
+    @State var backgroundColorSelected = ["2D00F7", "C1FF00", "FF206E", "480CA8", "FF6700", "FFFF00", "F10C45", "003AE7", "601EF9", "FF4500"]
     
     var body: some View {
 //        NavigationRouter {
